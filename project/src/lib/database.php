5971e91e121e50a15ca40f673a258e6284fabe72
Nicolas Emeriau <nicolas.emeriau@toolsvet.fr>
2024-04-13 11:53:29 +0200

    bdd ok

--- project/src/lib/database.php
+++ project/src/lib/database.php
@@ -9,7 +9,7 @@ class Database
     public function getConnexion(): \PDO
     {
         if ($this->database === null) {
-            $this->database = new \PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+            $this->database = new \PDO('mysql:host=db_docker_blog;dbname=blog;charset=utf8', 'root', '');
         }
 
         return $this->database;
